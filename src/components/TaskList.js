330502d12503c6bbfda6cf7a0a4bcacdd3dc0aee
Danny Little <dannyl9979@gmail.com>
2021-01-03 15:10:28 -0500

    better messages

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Task from './Task';
 import { getTasks } from '../api';
 
 const TaskList = ({ tasks, setTasks }) => {
   const { user } = useAuth0();
-
+  const [fetched, setFetched] = useState(false);
   const renderTasks = () => {
-    if (tasks.length > 0) {
-      return tasks.map(task => {
-        return <Task data={task} key={task.id} fakeDelete={fakeDelete} />;
-      });
+    if (fetched) {
+      if (tasks.length > 0) {
+        return tasks.map(task => {
+          return <Task data={task} key={task.id} fakeDelete={fakeDelete} />;
+        });
+      } else {
+        return <h4>You don't have any tasks right now. Impressive!</h4>;
+      }
     } else {
-      return <h4>You don't have any tasks right now. Impressive!</h4>;
+      return <p>loading...</p>;
     }
   };
 
@@ -24,6 +28,7 @@ const TaskList = ({ tasks, setTasks }) => {
     (async () => {
       try {
         const data = await getTasks(user.sub);
+        setFetched(true);
         setTasks(data);
       } catch (error) {
         console.error(error.message);
